5837ad29d85e9f7b46e9768038f9e685be1eec9e
inari6735 <piotrek1723@gmail.com>
2018-10-01 08:44:12 +0200

    bazy-zadania

---
+++ SQL/dane_uczniow/dane_uczniow.sql
@@ -0,0 +1,54 @@
+CREATE TABLE nazwiska(
+nr_ucznia INTEGER,
+nazwisko VARCHAR(30),
+imie1 VARCHAR(30),
+imie2 VARCHAR(30)
+);
+
+CREATE TABLE dane(
+dzien INTEGER,
+miesiac INTEGER,
+rok INTEGER,
+miasto_urodzenia TEXT,
+wojewodztwo TEXT
+);
+
+CREATE TABLE oceny(
+zachowanie VARCHAR(20),
+rel_et INTEGER,
+jpol INTEGER,
+jand INTEGER,
+jniem INTEGER,
+mat INTEGER,
+hist INTEGER,
+geog INTEGER,
+biol INTEGER,
+fiz INTEGER,
+che INTEGER,
+tech INTEGER,
+info INTEGER,
+plas INTEGER,
+po INTEGER,
+wf INTEGER
+);
+
+
+
+
+
+
+
+
+
+
+
+
+       / | \
+       \   /
+       |   |
+       |   |
+       |   |
+       |   |
+     / \   / \
+    |   | |   |
+     \ /   \ /
